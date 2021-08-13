8db397679c9f39b8cef48b582b098a426e92a1b0
zoe-joblin <85871720+zoe-joblin@users.noreply.github.com>
2021-08-13 12:54:30 +1200

    testing story routes

--- server.js
+++ server.js
@@ -17,48 +17,51 @@ server.set('view engine', 'hbs')
 //home route
 server.get('/', (request, response) => {
   response.render('landingview')
+ })
+
+ ////////*********//story route/////************** */
+server.get('/story', (request, response) => {
+  // response.sendFile(__dirname + '/story.json')
+  response.render('storyview')
+  // fs.readFile('/story.json', 'utf-8', (err, data) => {
+  //      res.render('storyview')
+})
+// })
+
+// /////////*********//post route/////************** */ (takes input from home page and displays on story page)
+// server.post('/', (request, response) => {
+//   const input = request.body 
+ 
+//   //reading existing story file and extracting data
+//   fs.readFile('/story.json', 'utf-8', (err, data) => {
+//      if (err) return err.message
+     
+//     const parsedData = JSON.parse(data)
   
 
- })
-
- //story route
-server.get('/story', (request, response) => {
-  response.render(__dirname + '/story.json')
-  // fs.readFile('./data.json', 'utf-8', (err, data) => {
-    //   //   if (err) return res.status(500).send(err.message)
-    //   //   res.render('view', data)
-})
-
-//post route (takes input from home page and displays on story page)
-server.post('/', (request, response) => {
-  const newWord = request.body 
- 
-  //reading story file
-  fs.readFile('/story.json', 'utf-8', (err, data) => {
-     if (err) return res.status(500).send(err.message)
-
-  const parsedData = JSON.parse(data)
-
-  // filter word functions
+//   // filter word functions
      
  
-  const newFileContents = JSON.stringify({ }, null, 2)
+//   // const newWord = JSON.stringify({ }, null, 2)
 
-  //add new word to story file
-      fs.writeFile('./data.json', newFileContents, 'utf-8', (err, data) => {
-      if (err) return res.status(500).send(err.message)
+//   //add new word to story file
+//     fs.append('/story.json', input, 'utf-8', (err) => {
+//       if (err) 
+//         return err.message
+      
+//         //display updated story
+//         response.redirect('/story)
+//       }
+//    })
+//  })
+// })
 
-      //display updated story
-     response.redirect('/story')
-   })
- })
 
-
-//about us route
-server.get('/about-us', (request, response) => {
-  response.sendFile(__dirname + 'aboutus.html')
+// //about us route
+// server.get('/about-us', (request, response) => {
+//   response.sendFile(__dirname + 'aboutus.html')
  
-})
+// })
 
 
 
