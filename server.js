bad0ff0589007c72e3d29b1e7374c46a9519c796
zoe-joblin <85871720+zoe-joblin@users.noreply.github.com>
2021-08-13 13:33:40 +1200

    refine post route

--- server.js
+++ server.js
@@ -21,40 +21,29 @@ server.get('/', (request, response) => {
 
  ////////*********//story route/////************** */
 server.get('/story', (request, response) => {
-  // response.sendFile(__dirname + '/story.json')
-  response.render('storyview')
-  // fs.readFile('/story.json', 'utf-8', (err, data) => {
-  //      res.render('storyview')
+  fs.readFile('/story.json', 'utf-8', (err, data) => {
+ 
+    response.render('storyview', data)
+ })
 })
-// })
 
 // /////////*********//post route/////************** */ (takes input from home page and displays on story page)
-// server.post('/', (request, response) => {
-//   const input = request.body 
- 
-//   //reading existing story file and extracting data
-//   fs.readFile('/story.json', 'utf-8', (err, data) => {
-//      if (err) return err.message
+server.post('/', (request, response) => {
+  const input = request.body 
+  fs.readFile('/story.json', 'utf-8', (err, data) => {
+     if (err) return err.message
      
-//     const parsedData = JSON.parse(data)
+    // const parsedData = JSON.parse(data)
+   // const newWord = JSON.stringify({ }, null, 2)
+
+    fs.append('/story.json', input, 'utf-8', (err) => {
+      if (err) return err.message
+        response.redirect('/story')
   
+    })
+ })
+})
 
-//   // filter word functions
-     
- 
-//   // const newWord = JSON.stringify({ }, null, 2)
-
-//   //add new word to story file
-//     fs.append('/story.json', input, 'utf-8', (err) => {
-//       if (err) 
-//         return err.message
-      
-//         //display updated story
-//         response.redirect('/story)
-//       }
-//    })
-//  })
-// })
 
 
 // //about us route
