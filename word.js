c7ce961162547cf4ddfd09875e6a6f226a8d0780
Jordy1311 <jordancooper1195@gmail.com>
2021-08-14 21:04:31 +1200

    WIP word validation checking function things

--- word.js
+++ word.js
@@ -4,35 +4,30 @@ const words = checkWord('en')
 
 const profanity = new Profanity({test: ''}, {language: 'en-us'});
 
-
-const checkTypeOf = (input) => {
-
-    return typeof input
+// input cannot be > 26
+const isTooLong = input => {
+    return input.length >= 26 ? true : false
 }
 
-const checkIfWord = (input) => {
-    return words.check(input)
+// input must be string type
+const isNotString = input => {
+    return typeof input !== "string" ? true : false
 }
 
-const checkIfProfane = (input) => {
-
-    return profanity.isProfane(input)
-
-}
-
-const checkIfTooLong = (input) => {
-
-    if(input.length >= 26)
-    {
-        return true;
+// if word is invalid return TRUE
+const isInvalid = input => {
+    if (isTooLong(input)) {
+        return true
+    } else if (isNotString(input)) {
+        return true
+    } else if (words.check(input) === false) {
+        return true
+    } else if (profanity.isProfane(input)) {
+        return true
     }
-    return false;
-
+    return false
 }
 
 module.exports = {
-    checkTypeOf: checkTypeOf,
-    checkIfWord: checkIfWord,
-    checkIfProfane: checkIfProfane,
-    checkIfTooLong: checkIfTooLong
+    isInvalid,
 }
